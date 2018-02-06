34da4e05c9351eacebfdc4477c75146b2e0eaff3
Amanda Wehrman <arwehrman@gmail.com>
2018-02-06 18:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
